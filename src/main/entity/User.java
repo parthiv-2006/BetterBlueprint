24a22cac8db9beb912364ccf2f559ea5699a3eb6
rachelzUT <ra.zhu@mail.utoronto.ca>
2025-11-10 16:30:24 -0500

    updated User entity

--- src/main/entity/User.java
+++ src/main/entity/User.java
@@ -1,12 +1,15 @@
 package main.entity;
 
 /**
- * A simple entity representing a user. Users have a username and password.
+ * A simple entity representing a user. Users have a username password, age, height, and weight.
  */
 
 public class User {
     private String username;
     private String password;
+    private int age;
+    private int height;
+    private int weight;
 
 
     /**
@@ -26,6 +29,21 @@ public class User {
         this.password = password;
     }
 
+    /**
+     * Creates a user with all attributes.
+     * @param username the username
+     * @param password the password
+     * @param age the user's age in years
+     * @param height the user's height
+     * @param weight the user's weight
+     */
+    public User(String username, String password, int age, int height, int weight) {
+        this(username, password);  // reuses validation logic
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
     public String getName() {
         return username;
     }
@@ -33,5 +51,17 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public int getAge() {return age;};
+
+    public void setAge(int age) {this.age = age;}
+
+    public int getHeight() {return height;}
+
+    public void setHeight(int height) {this.height = height;}
+
+    public int getWeight() {return weight;}
+
+    public void setWeight(int weight) {this.weight = weight;}
 
 }
