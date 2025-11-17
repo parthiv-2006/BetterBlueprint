916d751b9348b9bfe3bc17fff10c4a448b2ae21d
Hetvi Soni <Hetvisoni2909@gmail.com>
2025-11-17 16:45:39 -0500

    editted healthScoreHistory use case

--- src/main/java/use_case/healthScoreHistory/healthScoreHistoryInputBoundary.java
+++ src/main/java/use_case/healthScoreHistory/healthScoreHistoryInputBoundary.java
@@ -1,4 +1,11 @@
 package use_case.healthScoreHistory;
-
+/**
+ * The View Health Score History Use Case.
+ */
 public interface healthScoreHistoryInputBoundary {
+    /**
+     * Execute the View Health Score History Use Case.
+     * @param inputData the input data for this use case
+     */
+    void execute(healthScoreHistoryInputData inputData);
 }
