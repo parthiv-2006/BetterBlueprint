9fdcd7f2c8e9273a19749266b79b0218891281ad
Hetvi Soni <Hetvisoni2909@gmail.com>
2025-11-24 12:22:42 -0500

    changed UserDataInterface for healthScoreHistory

--- src/main/java/use_case/healthScoreHistory/healthScoreHistoryUserDataAccessInterface.java
+++ src/main/java/use_case/healthScoreHistory/healthScoreHistoryUserDataAccessInterface.java
@@ -1,4 +1,10 @@
 package use_case.healthScoreHistory;
 
+import Entities.HealthMetrics;
+import java.util.List;
+
 public interface healthScoreHistoryUserDataAccessInterface {
+    List<HealthMetrics> getHealthMetricsByUser(String userId);
+
+    String getCurrentUsername();
 }
